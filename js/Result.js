1793c13dcfa996d2111d97c9bfbf25fa33321e43
elviszhanglikeweb <467006015@qq.com>
2017-02-26 10:22:00 +0800

    v0.226

--- js/Result.js
+++ js/Result.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
     var h;
     var w;
+    var qwe;
+    qwe=1;
     w=document.documentElement.clientWidth||document.body.clientWidth;
     h=document.documentElement.clientHeight||document.body.clientHeight;
     $('body').css('height',h);
     $('body').css('width',w);
+    if(w<=414)
+    {
+        $(".header").fadeOut(1);
+        $(".rightarea").fadeOut(1);
+        $(".leftarea").fadeOut(1);
+        $(".mobile-2").fadeIn();
+    }
     var index=localStorage.getItem('index');   //利用split函数对cookies进行取值
     var test=localStorage.getItem('test'); // => 返回testKey对应的值
     if(test) { //判断session是否为空
@@ -20,6 +29,7 @@ $(document).ready(function () {
                 $count=1;
                 for(;$count<=$number;$count++){
                     $("div#div"+$count).fadeIn();
+                    $("div#divm"+$count).fadeIn();
                 }
                 //将软件的各种信息通过for函数查询出来
                 for($left=2,$right=1,$test=1,$start=0;$start<$number;$start++){
@@ -27,6 +37,14 @@ $(document).ready(function () {
                     document.getElementById("company"+$test).innerHTML=data.split(" ",$left++)[$right++];
                     document.getElementById("picture"+$test).setAttribute("src",data.split(" ",$left++)[$right++]);
                     document.getElementById("url"+$test).setAttribute("href",data.split(" ",$left++)[$right++]);
+                    $test++;
+                }
+                for($left=2,$right=1,$test=1,$start=0;$start<$number;$start++){
+                    document.getElementById("nm"+$test).innerHTML=data.split(" ",$left++)[$right++];
+                    document.getElementById("cm"+$test).innerHTML=data.split(" ",$left++)[$right++];
+                    document.getElementById("pm"+$test).setAttribute("src",data.split(" ",$left++)[$right++]);
+                    $left++;
+                    $right++;
                     $test++;
                 }
             }
@@ -45,12 +63,21 @@ $(document).ready(function () {
                 $count=1;
                 for(;$count<=$number;$count++){
                     $("div#div"+$count).fadeIn();
+                    $("div#divm"+$count).fadeIn();
                 }
                 for($left=2,$right=1,$test=1,$start=0;$start<$number;$start++){
                     document.getElementById("name"+$test).innerHTML=data.split(" ",$left++)[$right++];
                     document.getElementById("company"+$test).innerHTML=data.split(" ",$left++)[$right++];
                     document.getElementById("picture"+$test).setAttribute("src",data.split(" ",$left++)[$right++]);
                     document.getElementById("url"+$test).setAttribute("href",data.split(" ",$left++)[$right++]);
+                    $test++;
+                }
+                for($left=2,$right=1,$test=1,$start=0;$start<$number;$start++){
+                    document.getElementById("nm"+$test).innerHTML=data.split(" ",$left++)[$right++];
+                    document.getElementById("cm"+$test).innerHTML=data.split(" ",$left++)[$right++];
+                    document.getElementById("pm"+$test).setAttribute("src",data.split(" ",$left++)[$right++]);
+                    $left++;
+                    $right++;
                     $test++;
                 }
             }
@@ -64,5 +91,23 @@ $(document).ready(function () {
             window.location.reload();
         }
     });
-
+    $("button#check_mobile").click(function () {
+        if($("#content_mobile").val().toLowerCase()){
+            localStorage.removeItem('index');
+            localStorage.removeItem('test');
+            localStorage.setItem('test',$("#content_mobile").val().toLowerCase());
+            window.location.reload();
+        }
+    });
+    $("#search").click(function () {
+        if(qwe==1){
+            $("#search_div").fadeIn(500);
+            qwe=0;
+        }
+        else
+        {
+            $("#search_div").fadeOut(500);
+            qwe=1;
+        }
+    });
 });
