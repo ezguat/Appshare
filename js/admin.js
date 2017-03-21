57362e5a659c5a0f03c0d03978ad68d846042c51
elviszhanglikeweb <467006015@QQ.COM>
2017-03-21 20:00:28 +0800

    更新管理页面

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,3 @@
-/**
- * Created by jackz on 2017/3/14.
- */
 $(document).ready(function () {
     var h;
     var w;
@@ -8,5 +5,30 @@ $(document).ready(function () {
     h=document.documentElement.clientHeight||document.body.clientHeight;
     $('body').css('height',h);
     $('body').css('width',w);
+    $(function() {
+        var Accordion = function(el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
 
+            // Variables privadas
+            var links = this.el.find('.link');
+            // Evento
+            links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+        }
+
+        Accordion.prototype.dropdown = function(e) {
+            var $el = e.data.el;
+            $this = $(this),
+                $next = $this.next();
+
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+            };
+        };
+
+        var accordion = new Accordion($('#accordion'), false);
+    });
 });
