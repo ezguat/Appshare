9d1816282b7d24a3ee9ac4c96f25cb4fe5893260
elviszhanglikeweb <467006015@QQ.COM>
2017-03-25 17:29:49 +0800

    添加文件管理

--- js/admin.js
+++ js/admin.js
@@ -5,5 +5,14 @@ $(document).ready(function () {
     h=document.documentElement.clientHeight||document.body.clientHeight;
     $('body').css('height',h);
     $('body').css('width',w);
-
+    $.get("../php/admin.php",{tell:1},function (data) {
+        document.getElementById("ip").innerText=data.split(",")[0];
+        document.getElementById("browser").innerText=data.split(",")[1].split(":")[0];
+        document.getElementById("system").innerText=data.split(",")[2];
+        document.getElementById("time").innerText=data.split(",")[3];
+        $("#ip").css('color','black');
+        $("#time").css('color','black');
+        $("#browser").css('color','black');
+        $("#system").css('color','black');
+    });
 });
