735769c5cd34ce7aef96f30f439904d318616379
ezguat <467006015@qq.com>
2017-06-15 12:38:45 +0800

    debug

---
+++ js/advice.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    var h;
+    var w;
+    w=document.documentElement.clientWidth||window.screen.width;
+    h=document.documentElement.clientHeight||window.screen.height;
+    $('.desktop').css('height',h);
+    $('.desktop').css('width',w);
+    if(w<414)
+    {
+        $(".desktop").css('display','none');
+        $(".mobile").css('display','block');
+    }
+    var filter=/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    localStorage.setItem('time',new Date().toLocaleDateString());
+    var time1=localStorage.getItem('time');
+    $('button#myButton').click(function () {
+        if(filter.test($('#mail').val()))   //判断邮件是否按照的正则表达式
+        {
+            if($('#name').val()!=""&&$('#mail').val()!=""&&$('#advice').val()!=""){   //判断是否为空
+                $.get("../php/advice.php",{name:$('#name').val(),mail:$('#mail').val(),advice:$('#advice').val(),date:time1},function (data) {
+                    $('#warning').fadeOut();
+                    $('#succssful').fadeIn();
+                });
+            }
+            else
+            {
+                $('#succssful').fadeOut();
+                $('#warning').fadeIn();
+            }
+        }
+        else {
+            $('#succssful').fadeOut();
+            $('#warning').fadeIn();
+        }
+    });
+    localStorage.setItem('time',new Date().toLocaleDateString());
+    var time1=localStorage.getItem('time');
+    $('button#myButton_mobile').click(function () {
+        if(filter.test($('#mail_mobile').val()))   //判断邮件是否按照的正则表达式
+        {
+            if($('#name_mobile').val()!=""&&$('#mail_mobile').val()!=""&&$('#advice_mobile').val()!=""){   //判断是否为空
+                $.get("../php/advice.php",{name:$('#name_mobile').val(),mail:$('#mail_mobile').val(),advice:$('#advice_mobile').val(),date:time1},function (data) {
+                    $('#warning_mobile').fadeOut();
+                    $('#succssful_mobile').fadeIn();
+                });
+            }
+            else
+            {
+                $('#succssful_mobile').fadeOut();
+                $('#warning_mobile').fadeIn();
+            }
+        }
+        else {
+            $('#succssful_mobile').fadeOut();
+            $('#warning_mobile').fadeIn();
+        }
+    });
+});
